d6be573466c3bae022a09f2c28634422807bd90e
Patxi <patxi.madina@gmail.com>
2021-11-07 18:05:51 +0100

    Emacs: se incluye Magit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,13 +52,20 @@
   :config
   (ivy-mode 1))
 
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)
+         ("C-x C-g" . magit-status)))
 
-(custom-set-variables
+
+ (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (counsel ivy command-log-mode use-package))))
+ '(package-selected-packages
+   (quote
+    (magit doom-modeline counsel ivy command-log-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
