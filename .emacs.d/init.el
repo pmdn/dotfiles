258267263a9e85aea219d5cad1921c67e266cfb9
Patxi <patxi.madina@gmail.com>
2021-11-07 17:30:18 +0100

    Emacs: se incluye ivy y se hacen correcciones

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,12 +31,34 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package command-log-mode)
+
+(use-package counsel
+  ;;  :diminish
+  :demand
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (counsel ivy command-log-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
