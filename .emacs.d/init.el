0fe9056333e14b2ae55837a68613794b2d1f0780
Patxi <patxi.madina@gmail.com>
2021-11-08 22:16:51 +0100

    Emacs: bastantes modificaciones

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,15 @@
+;; Define the init file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; UTF-8 everywhere
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+;; User interface
 (menu-bar-mode -1)          ; Disable the menu bar
 (scroll-bar-mode -1)        ; Disable visible scrollbar
 (tool-bar-mode -1)          ; Disable the toolbar
@@ -7,7 +19,7 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
-
+;; Theme
 (load-theme 'wombat)
 
 ;; Make ESC quit prompts
@@ -31,10 +43,14 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package command-log-mode)
+;; Diminish minor modes
+(use-package diminish
+  :ensure t)
 
+;; Ivy / Counsel configuration
 (use-package counsel
-  ;;  :diminish
+  :diminish ivy-mode
+  :diminish counsel-mode
   :demand
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
@@ -50,29 +66,31 @@
          ("C-k" . ivy-previous-line)
          ("C-d" . ivy-reverse-i-search-kill))
   :config
-  (ivy-mode 1))
+  (ivy-mode 1)
+  (counsel-mode 1))
 
+;; Enrich Ivy
+(use-package ivy-rich
+  :after ivy
+  :init
+  (ivy-rich-mode 1))
+
+;; Magit for git
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)
          ("C-x C-g" . magit-status)))
 
+;; To show next commands
+(use-package which-key
+  :defer 0
+  :diminish which-key-mode
+  :config
+  (which-key-mode)
+  (setq which-key-idle-delay 0.5))
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
-
- (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (magit doom-modeline counsel ivy command-log-mode use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Back-up directories
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
