bf6440c069168c2f7d1d9f486aa1b58a567f52d7
Patxi <patxi.madina@gmail.com>
2021-11-09 11:21:31 +0100

    Emacs: se corrige parentesis de cierre

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -128,6 +128,6 @@
 ;; Prettier bullets
 (use-package org-bullets
   :ensure t
-  :hook (org-mode . org-bullets-mode)
+  :hook (org-mode . org-bullets-mode))
   ;;:custom
   ;;(org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
