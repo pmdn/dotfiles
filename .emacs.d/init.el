1c620ca7307ce663bf3d3cd9c3e4c2a49a60c48c
Patxi <pmdn@mailbox.org>
2022-03-24 20:36:41 +0100

    Incluyo org-roam-ui

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -388,6 +388,20 @@
   (deft-default-extension "org")
   (deft-directory org-roam-directory))
 
+(use-package org-roam-ui
+;;  :straight
+  ;;  (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+    :after org-roam
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start nil))
+
 ;; Magit for git
 (use-package magit
   :ensure t
