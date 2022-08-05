dbfd61adbb2449526eb0ada87f742d3e34a4fc4d
Patxi <pmdn@mailbox.org>
2022-08-05 12:43:51 +0200

    Quito projectile y me quedo con project.el (nativo) ~C-x p~

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -510,23 +510,6 @@
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
 
-;; Pojectile for working with projects
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  ;; NOTE: Set this to the folder where you keep your Git repos!
-  (when (file-directory-p "~/Projects/Code")
-    (setq projectile-project-search-path '("~/Projects/Code")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package counsel-projectile
-  :after projectile
-  :config (counsel-projectile-mode))
-
 ;; Easier to see if parenthesis are well closed
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
