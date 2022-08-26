80edf3b44e73ad58b30daa458b72145d65d1a82f
Patxi <pmdn@mailbox.org>
2022-08-26 17:35:19 +0200

    Configuro eshell y su prompt y modifico pyvenv, incl: instrucciones

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -496,6 +496,133 @@
            org-roam-ui-open-on-start nil))
      ))
 
+(use-package eshell
+  :init
+  (setq 
+        eshell-scroll-to-bottom-on-input 'all
+        eshell-scroll-show-maximum-output nil
+        eshell-error-if-no-glob t
+        eshell-hist-ignoredups t
+        eshell-save-history-on-exit t
+        eshell-prefer-lisp-functions nil
+        eshell-destroy-buffer-when-process-dies t)
+  ;;Eshell would get somewhat confused if I ran the following commands
+  (add-hook 'eshell-mode-hook
+            (lambda ()
+              (add-to-list 'eshell-visual-commands "ssh")
+              (add-to-list 'eshell-visual-commands "tail")
+              (add-to-list 'eshell-visual-commands "top"))))
+
+(defun curr-dir-git-branch-string (pwd)
+  "Returns current git branch as a string, or the empty string if
+PWD is not in a git repo (or the git command is not found)."
+  (interactive)
+  (when (and (not (file-remote-p pwd))
+             (eshell-search-path "git")
+             (locate-dominating-file pwd ".git"))
+    (let* ((git-url (shell-command-to-string "git config --get remote.origin.url"))
+           (git-output (shell-command-to-string (concat "git rev-parse --abbrev-ref HEAD")))
+           (git-branch (s-trim git-output))
+           (git-icon  "\xe0a0"))
+      (concat "[" git-branch "]"))))
+
+(defun pwd-replace-home (pwd)
+  "Replace home in PWD with tilde (~) character."
+  (interactive)
+  (let* ((home (expand-file-name (getenv "HOME")))
+         (home-len (length home)))
+    (if (and
+         (>= (length pwd) home-len)
+         (equal home (substring pwd 0 home-len)))
+        (concat "~" (substring pwd home-len))
+      pwd)))
+
+(defun pwd-shorten-dirs (pwd)
+  "Shorten all directory names in PWD except the last two."
+  (let ((p-lst (split-string pwd "/")))
+    (if (> (length p-lst) 2)
+        (concat
+         (mapconcat (lambda (elm) (if (zerop (length elm)) ""
+                               (substring elm 0 1)))
+                    (butlast p-lst 2)
+                    "/")
+         "/"
+         (mapconcat (lambda (elm) elm)
+                    (last p-lst 2)
+                    "/"))
+      pwd)))  ;; Otherwise, we just return the PWD
+
+(defun split-directory-prompt (directory)
+  (if (string-match-p ".*/.*" directory)
+      (list (file-name-directory directory) (file-name-base directory))
+    (list "" directory)))
+
+(defun python-prompt ()
+  "Returns a string (may be empty) based on the current Python
+   Virtual Environment. Assuming the M-x command: `pyvenv-activate'
+   has been called."
+  (when (not (eq pyvenv-virtual-env-name 'nil))
+    (concat "(" pyvenv-virtual-env-name ")")))
+
+(defun eshell/eshell-local-prompt-function ()
+  "A prompt for eshell that works locally (in that is assumes
+that it could run certain commands) in order to make a prettier,
+more-helpful local prompt."
+  (interactive)
+  (let* ((pwd        (eshell/pwd))
+         (directory (split-directory-prompt
+                     (pwd-shorten-dirs
+                      (pwd-replace-home pwd))))
+         (parent (car directory))
+         (name   (cadr directory))
+         (branch (curr-dir-git-branch-string pwd))
+         (python (when (not (file-remote-p pwd)) (python-prompt)))
+
+         (dark-env (eq 'dark (frame-parameter nil 'background-mode)))
+         (for-name  `(:weight bold :foreground "#81A1C1"))
+         (for-bars                 `(:weight bold))
+         (for-parent  (if dark-env `(:foreground "dark orange") `(:foreground "blue")))
+         (for-dir     (if dark-env `(:foreground "orange" :weight bold)
+                        `(:foreground "blue" :weight bold)))
+         (for-git                  `(:foreground "LightSeaGreen"))
+         (for-python               `(:foreground "DarkGrey")))
+
+    (concat
+     (propertize (user-login-name) 'face for-name)
+     (propertize "@" 'face for-name)
+     (propertize (system-name) 'face for-name)
+     (propertize " : " 'face for-bars)
+     (propertize parent   'face for-parent)
+     (propertize name     'face for-dir)
+     (when branch
+       (concat (propertize " : "    'face for-bars)
+               (propertize branch   'face for-git)))
+     (when python
+       (concat (propertize " : " 'face for-bars)
+               (propertize python 'face for-python)))
+     ;;(propertize "\n"     'face for-bars)
+     (propertize (if (= (user-uid) 0) " #" " $") 'face `(:weight ultra-bold))
+     ;; (propertize " └→" 'face (if (= (user-uid) 0) `(:weight ultra-bold :foreground "red") `(:weight ultra-bold)))
+     (propertize " "    'face `(:weight bold)))))
+
+(setq-default eshell-prompt-function #'eshell/eshell-local-prompt-function)
+
+(setq eshell-highlight-prompt nil)
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+    current buffer's file. The eshell is renamed to match that
+    directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((height (/ (window-total-height) 3)))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(bind-key "C-!" 'eshell-here)
+
 ;; Magit for git
 (use-package magit
   :ensure t
@@ -586,8 +713,6 @@
 ;; Pyvenv configuration
 (use-package pyvenv
   :ensure t
-  :init
-  (setenv "WORKON_HOME" "~/.pyenv/versions")
   :config
   (pyvenv-mode 1))
 
