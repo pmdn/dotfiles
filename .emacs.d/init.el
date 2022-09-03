fd7e11dcc08dd6a9bf8b3b8b50411797beb21057
Patxi <pmdn@mailbox.org>
2022-09-03 18:07:18 +0200

    Incluyo Marginalia

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -312,6 +312,20 @@
   ;; relevant when you use the default completion UI.
   :hook (completion-list-mode . consult-preview-at-point-mode))
 
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  ;; Either bind `marginalia-cycle' globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
 ;; Org mode configuration
 (defun efs/org-mode-setup ()
   (org-indent-mode 0)
