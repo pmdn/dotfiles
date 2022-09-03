d2d3f4a28f6fee37f57bce7ce7a5fc6ae1de0143
Patxi <pmdn@mailbox.org>
2022-09-03 18:12:21 +0200

    Añado Embark

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -326,6 +326,37 @@
   ;; enabled right away. Note that this forces loading the package.
   (marginalia-mode))
 
+(use-package embark
+  :ensure t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t
+  :after (embark consult)
+  :demand t ; only necessary if you have the hook below
+  ;; if you want to have consult previews as you move around an
+  ;; auto-updating embark collect buffer
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
 ;; Org mode configuration
 (defun efs/org-mode-setup ()
   (org-indent-mode 0)
