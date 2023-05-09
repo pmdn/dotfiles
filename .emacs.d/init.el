8b3f7cf323e938a2edd600c5a97dca7a50bc1e70
Patxi <pmdn@mailbox.org>
2023-05-09 20:54:28 +0200

    Incremento la anchura del modeline de 13 a 20 (default es 23)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -165,7 +165,7 @@
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 13)
+  :custom ((doom-modeline-height 20)
     (doom-modeline-buffer-file-name-style 'truncate-except-project)
     (doom-modeline-bar-width 6)))
 
